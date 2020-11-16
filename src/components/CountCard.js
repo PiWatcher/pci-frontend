9dc7e73342c47b2ab0e5f3c9c4f5685792be7392
Seth Burchfield <50897750+sethburchfield@users.noreply.github.com>
2020-11-16 11:08:59 -0700

    Initial commit of axios functionality

--- src/components/CountCard.js
+++ src/components/CountCard.js
@@ -3,30 +3,32 @@ import React, {Component} from 'react';
 
 
 
-class CountCard extends Component{
+class CountCard extends Component {
 
-  constructor() {
-      super()
+  // constructor for card component
+  constructor(props) {
+     
+		super(props)
 
-      this.state = {
-            
-          currentCount: 364
-
-      }
+		// sets the state passed down from axios
+		this.state = {
+		  currentCount: this.props.count
+		}
   }
-    
+	 
+  // renders the card with the passed data
   render () {
-      return (
-        <div className="CountCard">
-            <div className="text-container">
-                Current Count: 
-                <div className="num-container">
-                    {this.state.currentCount} / 1000
-                </div>
-            </div>
-        </div>
-      )
-    }
+		return (
+		  <div className="CountCard">
+				<div className="text-container">
+					 Current Count: 
+					 <div className="num-container">
+						  {this.state.currentCount} / 1000
+					 </div>
+				</div>
+		  </div>
+		)
+	 }
   }
 
 export default CountCard;
