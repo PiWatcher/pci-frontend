15ff90c39342638cb26985554e39d799688db221
Seth Burchfield <50897750+sethburchfield@users.noreply.github.com>
2020-11-16 11:45:02 -0700

    Edited code formatting

--- src/components/CountCard.js
+++ src/components/CountCard.js
@@ -8,27 +8,27 @@ class CountCard extends Component {
   // constructor for card component
   constructor(props) {
      
-		super(props)
+      super(props)
 
-		// sets the state passed down from axios
-		this.state = {
-		  currentCount: this.props.count
-		}
+      // sets the state passed down from axios
+      this.state = {
+        currentCount: this.props.count
+      }
   }
-	 
+    
   // renders the card with the passed data
   render () {
-		return (
-		  <div className="CountCard">
-				<div className="text-container">
-					 Current Count: 
-					 <div className="num-container">
-						  {this.state.currentCount} / 1000
-					 </div>
-				</div>
-		  </div>
-		)
-	 }
+      return (
+        <div className="CountCard">
+            <div className="text-container">
+                Current Count: 
+                <div className="num-container">
+                    {this.state.currentCount} / 1000
+                </div>
+            </div>
+        </div>
+      )
+    }
   }
 
 export default CountCard;
